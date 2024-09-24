28f3bd8e6fe37d8b199479c093e6ed0bd1d578f3
Scott Raddatz <raddatzscott@gmail.com>
2024-09-24 09:36:29 -0400

    Got rid of corner refernces

--- Assets/Prefabs/Enemies/EnemyMover.cs
+++ Assets/Prefabs/Enemies/EnemyMover.cs
@@ -42,12 +42,7 @@ public class EnemyMover : MonoBehaviour
     public bool ImMoving {get { return imMoving;}}
     bool imAttacking = false;
     public bool ImAttacking {get { return imAttacking;}}
-    public float cornerRadius = 3f;
-    private bool roundingCorner = false;
-    private Vector3 cornerCenter;
-    private float t = 0;
-    bool isThereACorner = false;
-
+ 
     
      private void Awake()
     {
@@ -60,9 +55,7 @@ public class EnemyMover : MonoBehaviour
     }
 
     void OnEnable()
-    {   t = 0;
-        isThereACorner = false;
-        roundingCorner = false;
+    {  
         imMoving = false;
         imAttacking = false; 
         last4Positions.Clear();
@@ -148,11 +141,8 @@ void RecalculatePath(bool resetPath)
             imAttacking = false; 
             Vector3 startPosition = transform.position;
             Vector3 endPosition = gridManager.GetPostitionFromCoordinates(path[i].coordinates);
-            Vector3 nextNode = gridManager.GetPostitionFromCoordinates(path[i+1].coordinates);
             targetPosition = endPosition;
             currentWaypoint = i;
-            distanceBetweenPoints = Vector3.Distance(startPosition, endPosition);
-            Corners(endPosition, nextNode);
 
             // add the next postiion to the list to track where its been, for mini spawns
             if(!last4Positions.Contains(gridManager.GetPostitionFromCoordinates(path[i].coordinates)))
@@ -170,29 +160,15 @@ void RecalculatePath(bool resetPath)
             transform.LookAt(endPosition);
 
             while (travelPercent < 1)
-            {   if(isThereACorner)
-                    {
-                        HowCloseToCornerStart(endPosition);
-                    }
-                    //if not currently rounding corner, follow path
-                if(!roundingCorner)
-                {
-                    travelPercent = elapsedTime/startSpeed/distanceBetweenPoints*10.10f;
-                    travelPercentOnDestroy = travelPercent;
-                    rb.MovePosition(Vector3.Lerp(startPosition, endPosition, travelPercent));
-                    yield return new WaitForEndOfFrame();
-                    elapsedTime += Time.deltaTime*speed;
-                }    
-                else{
-                    Debug.Log("I should start rounding the corner");
-                    StopAllCoroutines();
-                    StartCoroutine(GoAroundCorner(endPosition,gridManager.GetPostitionFromCoordinates(path[i+1].coordinates)));
-                    break;
-                    }                          
+            {   
+                travelPercent = elapsedTime/startSpeed/distanceBetweenPoints*10.10f;
+                travelPercentOnDestroy = travelPercent;
+                rb.MovePosition(Vector3.Lerp(startPosition, endPosition, travelPercent));
+                yield return new WaitForEndOfFrame();
+                elapsedTime += Time.deltaTime*speed;
+            }
         }
-        }
-        
-        // StartCoroutine(DamageCastle());
+        StartCoroutine(DamageCastle());
     }
 
 void OnTriggerEnter(Collider other) 
@@ -301,139 +277,6 @@ void UpdateSpeed()
         yield return new WaitForSeconds(0.25f);
         speed = 0f;
         imMoving = false;
-    }
-
-    // IEnumerator TurnCorner(Vector3 directionOfTurn)
-    // {
-    //     Vector3 startDirection = transform.forward;
-    //     Vector3 endDirection = directionOfTurn;
-        
-    //     float travelPercent = 0;
-    //     float elapsedTime = 0;
-
-    //     transform.LookAt(endPosition);
-
-    //     while (travelPercent < 1)
-    //     {   
-    //         travelPercent = elapsedTime/startSpeed/distanceBetweenPoints*10.10f;
-    //         travelPercentOnDestroy = travelPercent;
-    //         rb.MovePosition(Vector3.Lerp(startPosition, endPosition, travelPercent));
-    //         yield return new WaitForEndOfFrame();
-    //         elapsedTime += Time.deltaTime*speed;                               
-    //     }
-
-    // The points that make up the path
-    // public List<Vector3> path = new List<Vector3>();
-
-    // Radius of the corner rounding
- 
-
-    private void Corners(Vector3 closestNode, Vector3 nextNode)
-    {   // Get the current segment start and end
-        Vector3 startPoint = closestNode;
-        Vector3 endPoint = nextNode;
-
-        // Calculate direction between the points
-        Vector3 direction = (endPoint - startPoint).normalized;
-        Debug.Log("Angle of next two waypoints " + Vector3.Angle(transform.forward,direction));
-        if(Vector3.Angle(transform.forward,direction) > 5f)
-        {
-            isThereACorner = true;
-            CalculateCorner(startPoint, endPoint);
-            Debug.Log("The center of the corner is " + cornerCenter);
-            // Debug.Log("Distance to next end point " + Vector3.Distance(transform.position, startPoint));
-        }
-        else{isThereACorner = false;}
-    }
-    void HowCloseToCornerStart(Vector3 closestNode)
-    {
-        // // Check if we're approaching the end of the segment and should round the corner
-        if (!roundingCorner && Vector3.Distance(transform.position, closestNode) < cornerRadius && isThereACorner)
-        {
-            roundingCorner = true;           
-        }
-    }
-    IEnumerator GoAroundCorner(Vector3 first, Vector3 second)
-    {    
-        t = 0;
-        while(roundingCorner)
-        {
-        //     // Interpolate along the rounded corner
-                t += Time.deltaTime * speed / cornerRadius;
-                transform.position = GetRoundedCornerPosition(t);
-        //     // Rotate to face the next target point
-        //     Vector3 lookTarget = roundingCorner ? GetRoundedCornerPosition(t + 0.01f) : gridManager.GetPostitionFromCoordinates(path[currentWaypoint+1].coordinates);
-        //     transform.LookAt(lookTarget);
-        // Vector3 previousDirection = (first - transform.position).normalized;
-        // Vector3 nextDirection = (second - transform.position).normalized;
-        // Vector3 direction = Vector3.Lerp(previousDirection,nextDirection,t);
-        // Vector3 newPostiion = transform.position + speed * Time.fixedDeltaTime * direction;
-        // transform.LookAt(transform.position + direction);
-        // rb.MovePosition(newPostiion);
-
-        //     // Once we've finished rounding, move to the next segment
-            if (t >= Mathf.PI / 2f)
-            {   Debug.Log("DONE rounding the corner");
-                roundingCorner = false;
-                t = 0;
-                RecalculatePath(false);
-                break;
-            }
-            yield return new WaitForEndOfFrame();
-        }
-        // else
-        // {
-        // //     // Move along the straight line
-        //     transform.position = Vector3.MoveTowards(transform.position, endPoint, speed * Time.deltaTime);
-        // }
-    }
-
-
-    private void CalculateCorner(Vector3 startPoint, Vector3 endPoint)
-    {
-        // Calculate the direction to move out from the corner (bisect the angle)
-        Vector3 previousDirection = (transform.position - startPoint).normalized;
-        Vector3 nextDirection = (endPoint - startPoint).normalized;
-
-        Vector3 bisector = (previousDirection + nextDirection).normalized;
-        float angleBisectorToNext = Vector3.Angle(bisector,nextDirection)* Mathf.Deg2Rad;
-        float hypotenous = cornerRadius/Mathf.Sin(angleBisectorToNext);
-        cornerCenter = startPoint;
-        // determine quadrant based off bisectors x and z
-        int bisectorQuadrant;
-        if(bisector.x > 0 && bisector.z > 0) { bisectorQuadrant = 1;}
-        else if (bisector.x < 0 && bisector.z > 0) { bisectorQuadrant = 2;}
-        else if (bisector.x < 0 && bisector.z < 0) { bisectorQuadrant = 3;}
-        else    { bisectorQuadrant = 4;}
-
-        //find x and y component of corner center
-        switch(bisectorQuadrant)
-        {
-            case 1: cornerCenter.x += Mathf.Sin(angleBisectorToNext)*hypotenous;
-                    cornerCenter.z += Mathf.Cos(angleBisectorToNext)*hypotenous; break;
-
-            case 2: cornerCenter.x -= Mathf.Sin(angleBisectorToNext)*hypotenous;
-                    cornerCenter.z += Mathf.Cos(angleBisectorToNext)*hypotenous; break;
-
-            case 3: cornerCenter.x -= Mathf.Sin(angleBisectorToNext)*hypotenous;
-                    cornerCenter.z -= Mathf.Cos(angleBisectorToNext)*hypotenous; break;    
-
-            case 4: cornerCenter.x += Mathf.Sin(angleBisectorToNext)*hypotenous;
-                    cornerCenter.z -= Mathf.Cos(angleBisectorToNext)*hypotenous; break;    
-        }
-
-        // Debug.Log("The startpoint is "+startPoint + " and the hypotenous is " +hypotenous+" and the angle is " + angleBisectorToNext+
-        //            " and the bisector thing is " + bisector);
-        
-    }
-
-    private Vector3 GetRoundedCornerPosition(float angle)
-    {
-        // Calculate the position on the circular corner using parametric equations of a circle
-        float x = Mathf.Cos(angle) * cornerRadius;
-        float z = Mathf.Sin(angle) * cornerRadius;
-
-        return cornerCenter + new Vector3(x, 0, z); // Assumes a 2D XZ plane
     }
 
     private void OnDrawGizmos()
